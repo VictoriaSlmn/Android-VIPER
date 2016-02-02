7036d8715345c3c4532dedefc7590272487cbe65
v.suleymanova <v.suleymanova@rambler-co.ru>
2016-02-02 15:00:11 +0300

    remove default constructor in BasePresenter

--- sample/src/main/java/victoriaslmn/android/viper/sample/presentation/common/BasePresenter.java
+++ sample/src/main/java/victoriaslmn/android/viper/sample/presentation/common/BasePresenter.java
@@ -4,15 +4,6 @@ public abstract class BasePresenter<View, Router> {
     private View view;
     private Router router;
 
-    /**
-     * Default constructor.  <strong>Every</strong> presenter must have an
-     * empty constructor, so it can be instantiated when Fragment.onActivityCreated()
-     * It is strongly recommended that subclasses do not
-     * have other constructors with parameters
-     **/
-    public BasePresenter() {
-    }
-
     public abstract void onStart();
 
     public abstract void onStop();
